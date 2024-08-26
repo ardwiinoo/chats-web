4970adf58739a24262bf9daeaf8a2c2cd8bb154e
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-08-26 16:09:18 +0700

    feat: add web socket provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import AuthContextProvider from '@/modules/auth_provider'
+import WSProvider from '@/modules/ws_provider'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <AuthContextProvider>
-                <div className="flex flex-col md:flex-row h-full min-h-screen font-sans">
-                    <Component {...pageProps} />
-                </div>
+                <WSProvider>
+                    <div className="flex flex-col md:flex-row h-full min-h-screen font-sans bg-white">
+                        <Component {...pageProps} />
+                    </div>
+                </WSProvider>
             </AuthContextProvider>
         </>
     )

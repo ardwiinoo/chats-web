f7a35a0ec180bbdc288be3415a198040b5d25405
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-08-26 16:08:52 +0700

    feat: add web socket constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,4 @@
 export const API_URL =
     process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080'
+export const WEBSOCKET_URL =
+    process.env.NEXT_PUBLIC_WEBSOCKET_URL || 'ws://127.0.0.1:8080'
